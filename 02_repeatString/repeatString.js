b42595dfbda276d717044f85be326a54e570fa12
Adam Hartley <adamdhartley@icloud.com>
2022-09-28 14:54:45 -0400

    Solve exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else if (num === 0) {
+        return '';
+    }else {
+    let newString = string;    
+    for (let i = 1; i < num; i++) {
+        newString = newString.concat(string);
+        }
+        return newString
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
