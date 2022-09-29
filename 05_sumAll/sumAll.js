c33c03b955357970627b943ee04c531467dfebdd
Adam Hartley <adamdhartley@icloud.com>
2022-09-29 14:46:04 -0400

    Update solution with given solution in comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,3 +1,5 @@
+// My solution
+
 const sumAll = function(num1, num2) {
     let minNum = Math.min(num1, num2);
     let maxNum = Math.max(num1, num2);
@@ -16,3 +18,24 @@ const sumAll = function(num1, num2) {
 
 // Do not edit below this line
 module.exports = sumAll;
+
+/* Given solution - slight variation - better to test numbers first before executing any additional code
+
+const sumAll = function(min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  let sum = 0;
+  for (let i = min; i < max + 1; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+module.exports = sumAll;
+
+/*
