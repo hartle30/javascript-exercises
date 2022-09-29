8c3e3842d1e4a8f32ba5ef95c286173fb9809e2c
Adam Hartley <adamdhartley@icloud.com>
2022-09-29 14:39:59 -0400

    Solve exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let minNum = Math.min(num1, num2);
+    let maxNum = Math.max(num1, num2);
+    let sum = 0
+    if (typeof(num1) !== 'number' || typeof(num2) != 'number') {
+        return 'ERROR';
+    } else if (minNum < 0) {
+        return 'ERROR';
+    } else { 
+        for (let i = minNum; i <= maxNum; i++) {
+        sum += i;
+        }
+        return sum;
+    }   
 };
 
 // Do not edit below this line
