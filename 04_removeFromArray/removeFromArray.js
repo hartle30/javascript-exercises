9ebe442b0cae761e1789130b48988f9bf353a379
Adam Hartley <adamdhartley@icloud.com>
2022-09-29 13:39:29 -0400

    Solve exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(filterArray, ...args) {
+    const filteredArray = filterArray.filter(array => !args.includes(array));
+    return filteredArray
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
