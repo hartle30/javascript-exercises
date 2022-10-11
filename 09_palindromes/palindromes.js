669662015cc9f6baa5d2dd076ec336be03fceaf0
Adam Hartley <adamdhartley@icloud.com>
2022-10-11 15:15:49 -0400

    Solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lowerCaseString = string.toLowerCase();
+    const stringWithoutPunc = lowerCaseString
+        .replace(/[!@#$%^&*()_+-=\;',./{}|:"<>?']/g, '')
+        .replace(/\s{2, }/g, '')
+        .split(' ').join('');
+    console.log(stringWithoutPunc)
+    const reverseString = function(stringWithoutPunc) {
+        return stringWithoutPunc.split('').reverse().join('');
+    };
+    const stringInReverse = (reverseString(stringWithoutPunc));
+    if(stringWithoutPunc === stringInReverse) {
+        return true;
+    } return false;
 };
 
 // Do not edit below this line
