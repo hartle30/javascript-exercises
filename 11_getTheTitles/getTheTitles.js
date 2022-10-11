06451ddfc0be05893a178fa13a69c48f50cdd263
Adam Hartley <adamdhartley@icloud.com>
2022-10-11 16:35:04 -0400

    Update with given solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
+// My solution
+
 const getTheTitles = function(array) {
     return array.map(titles => `${titles.title}`);
 };
+
+/* Given solution - better name for map function and eliminates unnecessary template literal
+
+const getTheTitles = function(array) {
+    return array.map(book => book.title);
+};
+
+*/
 
 // Do not edit below this line
 module.exports = getTheTitles;
