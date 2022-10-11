bc25cbee5ec393abd9028f7246845f9e772ee0d5
Adam Hartley <adamdhartley@icloud.com>
2022-10-11 13:40:24 -0400

    Solve exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a;
+  for (let i = 1; i < b; i++) {
+    total = a * total;
+  };
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  } else {
+    let total = a;
+    for (let i = a; i >= 2; i--) {
+      total *= (i - 1);
+    };
+  return total;
+  };
 };
 
 // Do not edit below this line
