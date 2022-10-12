48830783d69b71b30146ae03c84c90fb29beb82f
Adam Hartley <adamdhartley@icloud.com>
2022-10-11 23:20:57 -0400

    Solve exercise with given solution in comments'

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+/* My original solution - I couldnt figure out how to find age for somebody still living
 
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(a, b) {
+        const last = a.yearOfDeath - a.yearOfBirth;
+        const next = b.yearOfDeath - b.yearOfBirth;
+        return last > next ? -1 : 1;
+    });
+    return oldest[0];
 };
+
+*/
+
+// Given Solution
+
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
+};
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    } 
+    return death - birth;
+};
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
